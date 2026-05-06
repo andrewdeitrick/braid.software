f47d67b7725991af53c0e2fec4104de9ad6bb44d
Andrew <104380316+andrewdeitrick@users.noreply.github.com>
2026-05-06 17:26:49 -0600

    feat(site): link checkout and mac download

---
+++ src/lib/links.ts
@@ -0,0 +1,5 @@
+export const BRAID_DOWNLOAD_URL =
+  "https://pub-0e602be4bbdc44d9a5ac321043dcab2f.r2.dev/braid/latest/Braid-aarch64.dmg";
+
+export const BRAID_PRO_CHECKOUT_URL =
+  "https://braid.lemonsqueezy.com/checkout/buy/71db74ab-61cd-4724-993c-451055363449";
